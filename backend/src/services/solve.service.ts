83c685a998882ad3026f524d29e614ec39015d52
maple <maple.devstack@gmail.com>
2026-09-12 01:16:38 +0530

    feat: update solveFormula to return solution instead of message

--- backend/src/services/solve.service.ts
+++ backend/src/services/solve.service.ts
@@ -1,8 +1,10 @@
+import solve from "../sat/solve.js"
 import type { Formula } from "../sat/types.js"
 
 export const solveFormula = (formula: Formula) => {
+  const solution = solve(formula)
+
   return {
-    message: "Solve service is working!",
-    formula,
+    solution,
   }
 }
